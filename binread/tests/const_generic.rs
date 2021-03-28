b298223d659665e51565fb93ec1989da116f85b7
jam1garner <8260240+jam1garner@users.noreply.github.com>
2021-03-28 13:32:01 -0400

    Add test for const generic array implementation

---
+++ binread/tests/const_generic.rs
@@ -0,0 +1,11 @@
+use binread::{BinRead, BinReaderExt, io::Cursor};
+
+#[test]
+fn const_generic_test() {
+    const IN: [u8; 300] = [6; 300];
+
+    let mut reader = Cursor::new(&IN);
+    let out: [u8; 300] = reader.read_be().unwrap();
+
+    assert_eq!(IN, out);
+}
